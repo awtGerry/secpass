c8a0bac9b2ffe41dcdfdbe735fbec2ec8a058a7b
Victor Rodriguez <awtgerry@gmail.com>
2024-05-17 23:41:16 -0600

    add cargo2nix

--- flake.nix
+++ flake.nix
@@ -1,70 +1,44 @@
 {
-  description = "Flake for iced";
+  description = "Iced example";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    cargo2nix.url = "github:cargo2nix/cargo2nix/release-0.11.0";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    crane,
-    flake-utils,
-    ...
-  }:
-    flake-utils.lib.eachDefaultSystem (system: let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-
-      buildInputs = with pkgs; [
-        vulkan-loader
-        # for wayland
-        wayland
-        wayland-protocols
-
-        # for x11
-        xorg.libX11
-        xorg.libXcursor
-        xorg.libXrandr
-        xorg.libXi
-
-        libxkbcommon
-      ];
-      my-crate = crane.lib.${system}.buildPackage {
-        src = ./.;
-        inherit buildInputs;
-
-        nativeBuildInputs = with pkgs; [
-          pkg-config
-          gtk-layer-shell
-          gtk3
-          cmake
-        ];
-      };
-    in {
-      checks = {
-        inherit my-crate;
-      };
-
-      packages.default = my-crate;
-
-      apps.default = flake-utils.lib.mkApp {
-        drv = my-crate;
-      };
-
-      devShells.default = pkgs.mkShell {
-        inputsFrom = builtins.attrValues self.checks;
-
-        LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-        # Extra inputs can be added here
-      };
-    });
+  outputs = inputs@{ self, nixpkgs, flake-parts, ...}: 
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      perSystem = {self', pkgs, system, ...}:
+        let
+          rustVersion = "1.76.0";
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [inputs.cargo2nix.overlays.default (import inputs.rust-overlay)];
+          };
+          rustPkgs = pkgs.rustBuilder.makePackageSet {
+            inherit rustVersion;
+            packageFun = import ./Cargo.nix;
+          };
+        in {
+          packages = rec {
+            iced_nix = (rustPkgs.workspace.iced_nix {}).bin;
+            default = iced_nix;
+          };
+          devShells.default = pkgs.mkShell rec {
+            buildInputs = with pkgs; [
+              pkg-config
+              wayland
+              libxkbcommon
+              libGL
+              
+              rust-analyzer-unwrapped
+              (rust-bin.stable.${rustVersion}.default.override { extensions = [ "rust-src" ]; })
+            ];
+            LD_LIBRARY_PATH = "${nixpkgs.lib.makeLibraryPath buildInputs}";
+          };
+        };
+    };
 }
